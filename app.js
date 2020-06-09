578819df113b8ba44f3505d4e02b1a2bc742e100
Michelle Ferreirae <mnfmnfm@gmail.com>
2020-06-09 13:31:30 -0700

    basic framework

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('JS linked!');
+$('#lognow').on('submit', function(e) {
+  e.preventDefault();
+  console.log('submitted');
+  console.log($(this).serialize());
+
+  let now = Date.now();
+  console.log(now);
+});
